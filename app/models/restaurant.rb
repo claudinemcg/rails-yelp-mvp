2bf4db26897cbb02453eaedd99c31efe709b4ef2
Claudine McGowan <claudine.mcgowan@gmail.com>
2020-10-10 13:08:07 +0100

    models, controllers, routes

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  validates :name, :address, presence: true
+  validates :category, presence: true
+
+  has_many :reviews, dependent: :destroy # anytime a restaurant is destroyed, so are the reviews
+end
